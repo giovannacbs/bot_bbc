46d7a3b7611d3b114d8e9a9b7038b47437a3b8dc
Your Name <giovannadcbs@gmail.com>
2024-04-04 20:31:40 -0300

    agoravaicerteza

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import requests 
 from flask import Flask, request
 import os
+import requests
+from bs4 import BeautifulSoup
 
 app = Flask(__name__)
 
@@ -17,16 +19,37 @@ def enviar_mensagem(chat_id, texto):
 
 # Definindo as saídas de acordo com a escolha do usuário
 def brasil():
-    return "Você escolheu a opção 1"
+    tema = 'cz74k717pw5t'  
+    noticias = exibir_top5(tema)
+    resposta = "Últimas notícias sobre o Brasil:\n"
+    for titulo, link in noticias:
+        resposta += f"{titulo}\nLink: https://www.bbc.com{link}\n\n"
+    return resposta
 
 def internacional():
-    return "Você escolheu a opção 2"
+    tema = 'cmdm4ynm24kt'  
+    noticias = exibir_top5(tema)
+    resposta = "Últimas notícias internacionais:\n"
+    for titulo, link in noticias:
+        resposta += f"{titulo}\nLink: https://www.bbc.com{link}\n\n"
+    return resposta
 
-def politica():
-    return "Você escolheu a opção 3"
+def tecnologia():
+    tema = 'c404v027pd4t'  
+    noticias = exibir_top5(tema)
+    resposta = "Últimas notícias sobre tecnologia:\n"
+    for titulo, link in noticias:
+        resposta += f"{titulo}\nLink: https://www.bbc.com{link}\n\n"
+    return resposta
 
 def economia():
-    return "Você escolheu a opção 4"
+    tema = 'cmdm4ynm24kt'  
+    noticias = exibir_top5(tema)
+    resposta = "Últimas notícias de Economia:\n"
+    for titulo, link in noticias:
+        resposta += f"{titulo}\nLink: https://www.bbc.com{link}\n\n"
+    return resposta
+
 
 # Criando a rota para manter o bot funcionando e criando menu com as opções
 @app.route('/mensagem', methods=['POST'])
@@ -36,13 +59,13 @@ def mensagem():
     texto = mensagem['message']['text']
     
     if texto == '/start':
-        resposta = "Escolha uma opção:\n1. Brasil\n2. Internacional\n3. Política\n4. Economia"
+        resposta = "Escolha uma opção e responda com o número selecionado:\n1. Brasil\n2. Internacional\n3. Política\n4. Economia"
     elif texto == '1':
         resposta = brasil()
     elif texto == '2':
         resposta = internacional()
     elif texto == '3':
-        resposta = politica()
+        resposta = tecnologia()
     elif texto == '4':
         resposta = economia()
     else:
@@ -53,3 +76,29 @@ def mensagem():
 
 if __name__ == '__main__':
     app.run(threaded=True)
+
+def exibir_top5(tema):
+  url = 'https://www.bbc.com/portuguese/topics/' + tema
+
+  response = requests.get(url)
+  soup = BeautifulSoup(response.text, 'html.parser')
+
+  h2 = soup.find_all('h2') # look for all titles 
+  titulos = []
+  for h in h2:
+    titulo = h.get_text()
+    titulos.append(titulo)
+
+  titulos = titulos[:5] # select only first 5
+  
+  links = soup.find_all('a', {'class' : 'focusIndicatorDisplayBlock bbc-uk8dsi e1d658bg0'})
+  
+  hrefs = []
+  for href in links:
+    hrefs.append(href.get('href'))
+
+  hrefs = hrefs[:5]
+
+  materias = list(zip(titulos, hrefs))
+
+  return materias
